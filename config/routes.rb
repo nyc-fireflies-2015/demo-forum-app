822fa714446d4d203148d68bb436e5b324b8d2bb
stevecass <steven@mumsnet.com>
2015-04-07 09:22:04 -0400

    Add topic listing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  resources :topics
+  root 'topics#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
