5a227bd33ed9e21f4dd6638d9cea4273963e502f
stevecass <steven@mumsnet.com>
2015-04-07 11:15:12 -0400

    Fix reference to threads

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,6 @@
 class ConversationsController < ApplicationController
 
   def show
-    @thread = Conversation.find(params[:id])
+    @conv = Conversation.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :threads, foreign_key: :author_id
+  has_many :conversations, foreign_key: :author_id
   has_many :messages, foreign_key: :author_id
   has_secure_password
 end
