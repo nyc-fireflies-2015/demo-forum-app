e7c4095810fa3ac59e6a65038ae1027141447cb8
Steven Cassidy <steven.cassidy@gmail.com>
2015-09-06 20:19:47 -0400

    Add feedback to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,24 +10,27 @@ users = []
 10.times do
   users << User.create!(username:Faker::Internet.user_name, email:Faker::Internet.email, password:'123456')
 end
-6.times do
+16.times do
   top = Topic.create!(name: Faker::Company.catch_phrase)
+  puts "Creating topic #{top.name}"
   6.times do
-    thr_args = { subject:Faker::Commerce.product_name, 
-                 topic:top, 
-                 author:users.sample, 
-                 is_public:[true, false].sample, 
+    thr_args = { subject:Faker::Commerce.product_name,
+                 topic:top,
+                 author:users.sample,
+                 is_public:[true, false].sample,
                  is_published:[true, false].sample
                 }
     thr = Conversation.create!(thr_args)
     (5 + Random.rand(20)).times do
-      msg_args ={ author:users.sample, 
-                  conversation:thr, 
-                  content:Faker::Lorem.paragraph(3), 
-                  is_public:[true, false].sample, 
+      msg_args ={ author:users.sample,
+                  conversation:thr,
+                  content:Faker::Lorem.paragraph(3),
+                  is_public:[true, false].sample,
                   is_published:[true, false].sample
                 }
       msg = Message.create!(msg_args)
+      print "."
     end
   end
+  puts
 end
