27607ca12fc4fefa74c767d2d7c976be4200f5c1
Steven Cassidy <steven.cassidy@gmail.com>
2015-09-06 20:31:37 -0400

    Track target page on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,22 @@
 class SessionsController < ApplicationController
   skip_before_filter :ensure_current_user
 
+  def new
+    @target_page = params[:target_page]
+  end
+
   def create
     user = User.find_by(username: params[:username])
     puts 'User not located' if !user
     if user && user.authenticate(params[:password])
       puts "We logged in as user #{user.id}"
       session[:user_id] = user.id
-      redirect_to root_path
+      if params[:target_page]
+        redirect_to params[:target_page]
+      else
+        redirect_to root_path
+      end
+
     else
       render 'new'
     end
@@ -17,5 +26,5 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     session.destroy if session
     redirect_to root_path
-  end  
+  end
 end
